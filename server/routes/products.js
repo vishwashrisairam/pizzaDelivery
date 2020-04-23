33fe446dafcade5da60c97273919f0e8ecdd9a9e
mahesh-9 <umamaheshnov9@gmail.com>
2020-04-23 01:12:55 -0500

    added delete route

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { getProducts, createProducts } = require('../controllers/products');
+const { getProducts, createProducts, deleteProducts } = require('../controllers/products');
 
 const router = express.Router();
 
 router.get('/products', getProducts);
 router.post('/products', createProducts);
+router.patch('/products/:id', deleteProducts);
 
 module.exports = router;
