fcc648cde1b6bde03e07f6e67ba68e6fafc392d1
mahesh-9 <umamaheshnov9@gmail.com>
2020-05-07 22:01:35 -0500

    added orders

--- models/order.js
+++ models/order.js
@@ -1,23 +1,49 @@
 const mongoose = require("mongoose");
 
+
+const productSchema = new mongoose.Schema({
+    productName: String,
+    category: String,
+    description: String,
+    price: String,
+    size: String,
+    crust: String,
+    toppings: String,
+    image: String,
+    extra: [
+        {
+            ingredient: String,
+        }
+    ],
+    quantity: {type:Number,default: 1}
+});
+
+
 const orderSchema = new mongoose.Schema({
-    _user: {
+    name:{type: String, default: "Mahesh"},
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
-    items: [
-        {
-            _product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Products'
-            },
-            quantity: {type:Number,default: 1}
-        }
-    ],
-    totalPrice: String,
+    items: {type :[productSchema], default:[]},
+    deliveryAddress:{
+        addressLine1:String,
+        addressLine2:String,
+        city:String,
+        state:String,
+        zip:String,
+        country:String
+    },
+    paymentDetails:{
+        paymentMode:String,
+        cardNumber:String,
+        cardName:String,
+        expirationDate:String
+    },
+    orderAmount: String,
     feedbackRating: Number,
-    status: String,
-    date: Date
+    status:{type:String, default: "Placed"},
+    date: {type:Date, default:() => Date.now()}
 });
 
 module.exports = mongoose.model('Order', orderSchema);
